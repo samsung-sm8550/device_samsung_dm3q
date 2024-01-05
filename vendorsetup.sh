7d8f757e0b606b315c20984266443bbea0d6a595
Edgars Cīrulis <edgarsscirulis@gmail.com>
2024-01-05 03:46:17 +0200

    dm3q: Orangefox Bringup
    Signed-off-by: Edgars Cīrulis <edgarsscirulis@gmail.com>

---
+++ vendorsetup.sh
@@ -0,0 +1,41 @@
+#
+#	This file is part of the OrangeFox Recovery Project
+# 	Copyright (C) 2020-2023 The OrangeFox Recovery Project
+#
+#	OrangeFox is free software: you can redistribute it and/or modify
+#	it under the terms of the GNU General Public License as published by
+#	the Free Software Foundation, either version 3 of the License, or
+#	any later version.
+#
+#	OrangeFox is distributed in the hope that it will be useful,
+#	but WITHOUT ANY WARRANTY; without even the implied warranty of
+#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#	GNU General Public License for more details.
+#
+# 	This software is released under GPL version 3 or any later version.
+#	See <http://www.gnu.org/licenses/>.
+#
+# 	Please maintain this if you use this script or any part of it
+#
+
+export ALLOW_MISSING_DEPENDENCIES="true"
+export OF_DISABLE_MIUI_SPECIFIC_FEATURES="1"
+export OF_DONT_PATCH_ENCRYPTED_DEVICE="1"
+export OF_SCREEN_H="2400"
+export OF_STATUS_H="100"
+export OF_STATUS_INDENT_LEFT="48"
+export OF_STATUS_INDENT_RIGHT="48"
+export OF_USE_MAGISKBOOT="1"
+export OF_USE_MAGISKBOOT_FOR_ALL_PATCHES="1"
+export OF_NO_TREBLE_COMPATIBILITY_CHECK="1"
+export OF_SKIP_MULTIUSER_FOLDERS_BACKUP="1"
+export LC_ALL="C"
+export FOX_USE_NANO_EDITOR="1"
+export FOX_DELETE_AROMAFM="1"
+export FOX_USE_TAR_BINARY="1"
+export FOX_USE_SED_BINARY="1"
+export FOX_USE_TWRP_RECOVERY_IMAGE_BUILDER="1"
+export FOX_USE_XZ_UTILS="1"
+export FOX_ENABLE_APP_MANAGER="1"
+export OF_MAINTAINER="Edgars Cirulis"
+export FOX_BUILD_TYPE="Beta"
